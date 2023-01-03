5eb99b1907ba030df117f38aa6853efaa0af6d8a
Akshaykumar S H <76952392+Akshay251@users.noreply.github.com>
2023-01-03 12:26:16 +0530

    Update script.js
    updated

--- script.js
+++ script.js
@@ -2,18 +2,16 @@ var yes = document.getElementById("yes");
 var no = document.getElementById("no");
 var answer = document.getElementsByClassName("answer");
 
-yes.addEventListener("mouseover",function(){
-	/*let answerpos = answer.getBoundingClientRect();*/
+no.addEventListener("mouseover",function(){
+	alert("I knew it from the start ;)")
+});
+
+yes.addEventListener("click", function(){
 	let nopos = no.getBoundingClientRect();
-	/*let offset = nopos.top - answerpos.top;*/
 	if(nopos.top <= 290.875){
 		no.style.transform = "translateY(100px)"
 	}
 	if(nopos.top > 290.875){
 		no.style.transform = "translateY(-100px)"
 	}
-});
-
-no.addEventListener("click", function(){
-	alert("I knew it from the start ;)")
 })
