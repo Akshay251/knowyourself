050893d2c6f65a627402a5ca046bdeb596796fa3
Akshaykumar S H <76952392+Akshay251@users.noreply.github.com>
2023-01-03 12:29:44 +0530

    Update script.js
    updated1

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var yes = document.getElementById("yes");
 var no = document.getElementById("no");
 var answer = document.getElementsByClassName("answer");
 
-no.addEventListener("mouseover",function(){
+no.addEventListener("click",function(){
 	alert("I knew it from the start ;)")
 });
 
-yes.addEventListener("click", function(){
+yes.addEventListener("mouseover", function(){
 	let nopos = yes.getBoundingClientRect();
 	if(nopos.top <= 290.875){
 		yes.style.transform = "translateY(100px)"
