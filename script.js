7a4dc77f90cdd2189ee9266bf8c1e4e678b994cd
Akshaykumar S H <76952392+Akshay251@users.noreply.github.com>
2023-03-11 17:52:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ no.addEventListener("click",function(){
 	alert("I knew it from the start ;)")
 });
 
-yes.addEventListener("click", function(){
+yes.addEventListener("onmouseover", function(){
 	let nopos = yes.getBoundingClientRect();
 	if(nopos.top <= 290.875){
 		yes.style.transform = "translateY(100px)"
